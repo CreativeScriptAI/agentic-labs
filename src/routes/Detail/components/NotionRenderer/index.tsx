e990ccf918ac1b0ac166081e3cb0ef90d159b9c9
echo-outlier <yashshivhare@hotmail.com>
2025-08-08 04:24:13 +0530

    removed scheme

--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -1,65 +1,64 @@
-import dynamic from "next/dynamic"
-import Image from "next/image"
-import Link from "next/link"
-import { ExtendedRecordMap } from "notion-types"
-import useScheme from "src/hooks/useScheme"
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import Link from "next/link";
+import { ExtendedRecordMap } from "notion-types";
+import useScheme from "src/hooks/useScheme";
 
 // core styles shared by all of react-notion-x (required)
-import "react-notion-x/src/styles.css"
+import "react-notion-x/src/styles.css";
 
 // used for code syntax highlighting (optional)
-import "prismjs/themes/prism-tomorrow.css"
+import "prismjs/themes/prism-tomorrow.css";
 
 // used for rendering equations (optional)
 
-import "katex/dist/katex.min.css"
-import { FC } from "react"
-import styled from "@emotion/styled"
+import "katex/dist/katex.min.css";
+import { FC } from "react";
+import styled from "@emotion/styled";
 
 const _NotionRenderer = dynamic(
   () => import("react-notion-x").then((m) => m.NotionRenderer),
   { ssr: false }
-)
+);
 
 const Code = dynamic(() =>
   import("react-notion-x/build/third-party/code").then(async (m) => m.Code)
-)
+);
 
 const Collection = dynamic(() =>
   import("react-notion-x/build/third-party/collection").then(
     (m) => m.Collection
   )
-)
+);
 const Equation = dynamic(() =>
   import("react-notion-x/build/third-party/equation").then((m) => m.Equation)
-)
+);
 const Pdf = dynamic(
   () => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf),
   {
     ssr: false,
   }
-)
+);
 const Modal = dynamic(
   () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
   {
     ssr: false,
   }
-)
+);
 
 const mapPageUrl = (id: string) => {
-  return "https://www.notion.so/" + id.replace(/-/g, "")
-}
+  return "https://www.notion.so/" + id.replace(/-/g, "");
+};
 
 type Props = {
-  recordMap: ExtendedRecordMap
-}
+  recordMap: ExtendedRecordMap;
+};
 
 const NotionRenderer: FC<Props> = ({ recordMap }) => {
-  const [scheme] = useScheme()
   return (
     <StyledWrapper>
       <_NotionRenderer
-        darkMode={scheme === "dark"}
+        darkMode={false}
         recordMap={recordMap}
         components={{
           Code,
@@ -73,10 +72,10 @@ const NotionRenderer: FC<Props> = ({ recordMap }) => {
         mapPageUrl={mapPageUrl}
       />
     </StyledWrapper>
-  )
-}
+  );
+};
 
-export default NotionRenderer
+export default NotionRenderer;
 
 const StyledWrapper = styled.div`
   /* // TODO: why render? */
@@ -89,4 +88,4 @@ const StyledWrapper = styled.div`
   .notion-list {
     width: 100%;
   }
-`
+`;
