dab91d2ec00db2bd42db430d4635f0308cd12308
echo-outlier <yashshivhare@hotmail.com>
2025-08-10 05:56:30 +0530

    testing amplify

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  trailingSlash: true,
   images: {
     domains: ["www.notion.so", "notion.so"],
     unoptimized: true,
