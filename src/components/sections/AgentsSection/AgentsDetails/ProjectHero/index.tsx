6d7a7a1a7aa52dc76ca25554b8c3c294a05fd070
Loordhu Jeyakumar <loordhujeyakumar@gmail.com>
2025-09-04 22:42:28 +0530

    refactor ProjectHero component by removing unused loading state and skeleton loader, streamlining the rendering logic

--- src/components/sections/AgentsSection/AgentsDetails/ProjectHero/index.tsx
+++ src/components/sections/AgentsSection/AgentsDetails/ProjectHero/index.tsx
@@ -16,26 +16,8 @@ interface ProjectHeroProps {
 }
 
 const ProjectHero = ({ data }: ProjectHeroProps) => {
-  const isVideo = data.heroImage.endsWith(".mp4");
-  const [isLoading, setIsLoading] = useState(true);
 
-  const handleLoad = () => {
-    setIsLoading(false);
-  };
 
-  const SkeletonLoader = () => (
-    <div className="w-[300px] h-[300px] max-w-[440px] aspect-square rounded-lg bg-gray-200 animate-pulse flex items-center justify-center">
-      <div className="h-20 w-20text-gray-400">
-        <svg className="w-full h-full" fill="currentColor" viewBox="0 0 20 20">
-          <path
-            fillRule="evenodd"
-            d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </div>
-    </div>
-  );
 
   return (
     <section className="section text-center mt-[132px]">
@@ -50,62 +32,6 @@ const ProjectHero = ({ data }: ProjectHeroProps) => {
         {data.description}
       </p>
       <AICaller />
-      <div className="flex flex-col lg:flex-row items-center justify-center gap-8 lg:gap-12">
-        <ul className="flex flex-col items-start md:items-start gap-2 md:gap-4 font-sfpro text-xs md:text-sm text-slate-800 font-medium lg:w-1/2">
-          {data.benefits.map((benefit, index) => (
-            <li key={index} className="flex items-center gap-1 text-slate-600">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="19"
-                  height="20"
-                  viewBox="0 0 19 20"
-                  fill="none"
-                >
-                  <path
-                    d="M9.03409 19.0342C14.0235 19.0342 18.0682 14.9895 18.0682 10.0001C18.0682 5.0107 14.0235 0.966003 9.03409 0.966003C4.0447 0.966003 0 5.0107 0 10.0001C0 14.9895 4.0447 19.0342 9.03409 19.0342Z"
-                    fill="#0062FF"
-                  />
-                  <path
-                    d="M13.0994 7.2901L7.50733 12.4666L4.96875 10.1178"
-                    stroke="white"
-                    strokeWidth="1.80682"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </div>
-              <div className="text-left">
-                <span className="text-slate-600">{benefit.text}</span>
-              </div>
-            </li>
-          ))}
-        </ul>
-        <div className="flex flex-col items-center gap-5 md:gap-6 lg:w-1/2">
-          <div className="relative">
-            {isLoading && <SkeletonLoader />}
-            {isVideo ? (
-              <video
-                className={`w-full max-w-[440px] rounded-lg transition-opacity duration-300 ${
-                  isLoading ? "opacity-0 absolute inset-0" : "opacity-100"
-                }`}
-                autoPlay
-                controlsList="nodownload noplaybackrate"
-                controls
-                disablePictureInPicture
-                loop
-                playsInline
-                onLoadedData={(e) => {
-                  (e.target as HTMLVideoElement).volume = 0;
-                  handleLoad();
-                }}
-              >
-                <source src={data.heroImage} type="video/mp4" />
-              </video>
-            ) : null}
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
